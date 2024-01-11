4cde81705df0d04e1b0b42c948bdccbfcca016b9
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2024-01-11 22:25:43 +0300

    Create 1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Module 1-concat
+"""
+
+
+import string
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates 2 strings
+    """
+    return str1+str2

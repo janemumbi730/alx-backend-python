6b4645d011f677358589093e6579623bd7d13457
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2024-01-11 22:51:21 +0300

    Update 100-safe_first_element.py

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -5,7 +5,6 @@ Arugment the following code with the
 from typing import TypeVar, Any, Union, Mapping, Sequence
 
 
-# The types of the elements of the input are not know
 def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     """func that returns the first element if an iterable is passed, else None"""
     if lst:

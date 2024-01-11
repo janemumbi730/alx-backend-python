af95ff0bb2b7a699a24cc4191056bbd406684fd6
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2024-01-11 22:30:26 +0300

    Create 5-sum_list.py

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+Module 5-sum_list
+"""
+
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    takes list input_list floats
+    """
+    return sum(input_list)

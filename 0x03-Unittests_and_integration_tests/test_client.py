ad3627ebe86c1a48df4d58f6d10b8a19782b8372
Mumbi <122810317+janemumbi730@users.noreply.github.com>
2024-02-05 15:23:33 +0300

    Update test_client.py

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,26 +1,113 @@
 #!/usr/bin/env python3
-""" Unittest module """
+"""
+Module test_client
+"""
+from parameterized import parameterized, parameterized_class
+import unittest
+from unittest.mock import Mock, PropertyMock
+from unittest.mock import patch
 
-from unittest import TestCase, mock
-from unittest.mock import patch, Mock
-from parameterized import parameterized
-
-import client
 from client import GithubOrgClient
+from fixtures import TEST_PAYLOAD
 
 
-class TestGithubOrgClient(TestCase):
-    """ Class for testing GithubOrgClient """
+class TestGithubOrgClient(unittest.TestCase):
+    """Test Class"""
 
     @parameterized.expand([
         ("google"),
         ("abc"),
-    ])
-    @patch('client.get_json')
-    def test_org(self, org_name, mock_json):
-        """ Test method returns correct output """
-        gc = GithubOrgClient(org_name)
-        gc.org()
-        mock_json.assert_called_once_with(
-            f"https://api.github.com/orgs/{org_name}"
-            )
+        ])
+    @patch("client.get_json")
+    def test_org(self, org, mock_org):
+        """Tests GithubOrgClient.org"""
+        client_org = GithubOrgClient(org)
+        respons = client_org.org
+        self.assertEqual(respons, mock_org.return_value)
+        mock_org.assert_called_once()
+
+    def test_public_repos_url(self):
+        """Tests GithubOrgClient._public_repos_url"""
+        with patch.object(GithubOrgClient,
+                          "org",
+                          new_callable=PropertyMock) as p:
+            p.return_value = {"repos_url": "89"}
+            client_org = GithubOrgClient("holberton")
+            test_repo_url = client_org._public_repos_url
+            self.assertEqual(test_repo_url, p.return_value.get('repos_url'))
+            p.assert_called_once()
+
+    @patch('client.get_json', return_value=[{'name': 'Holberton'},
+                                            {'name': '89'},
+                                            {'name': 'alx'}])
+    def test_public_repos(self, mock_repo):
+        """
+        Test GithubOrgClient's public_repos method
+        """
+        with patch.object(GithubOrgClient,
+                          '_public_repos_url',
+                          new_callable=PropertyMock,
+                          return_value="https://api.github.com/") as m:
+            test_client = GithubOrgClient('holberton')
+            test_repo = test_client.public_repos()
+            for idx in range(3):
+                self.assertIn(mock_repo.return_value[idx]['name'], test_repo)
+            mock_repo.assert_called_once()
+            m.assert_called_once()
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+        ])
+    def test_has_license(self, repo, license_key, expected):
+        """Tests GithubOrgClient.has_license"""
+        client_org = GithubOrgClient("holberton")
+        licens_e = client_org.has_license(repo, license_key)
+        self.assertEqual(licens_e, expected)
+
+
+@parameterized_class(
+    ('org_payload', 'repos_payload', 'expected_repos', 'apache2_repos'),
+    [(TEST_PAYLOAD[0][0], TEST_PAYLOAD[0][1], TEST_PAYLOAD[0][2],
+      TEST_PAYLOAD[0][3])]
+)
+class TestIntegrationGithubOrgClient(unittest.TestCase):
+    """
+    Integration test for the GithubOrgClient.public_repos method
+    """
+    @classmethod
+    def setUpClass(cls):
+        """
+        Set up function for TestIntegrationGithubOrgClient class
+        Sets up a patcher to be used in the class methods
+        """
+        cls.get_patcher = patch('utils.requests.get', side_effect=requests_get)
+        cls.get_patcher.start()
+        cls.client = GithubOrgClient('google')
+
+    @classmethod
+    def tearDownClass(cls):
+        """
+        Tear down resources set up for class tests.
+        Stops the patcher that had been started
+        """
+        cls.get_patcher.stop()
+
+    def test_public_repos(self):
+        """
+        Test public_repos method without license
+        """
+        self.assertEqual(self.client.public_repos(), self.expected_repos)
+
+    def test_public_repos_with_license(self):
+        """
+        Test public_repos method with license
+        """
+        self.assertEqual(
+            self.client.public_repos(license="apache-2.0"),
+            self.apache2_repos)
+
+
+if __name__ == "__main__":
+    """Entry point"""
+    unittest.main()
